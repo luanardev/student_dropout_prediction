f09ba19241e6b2c7944d11d4d9c6a6228bf60749
Brian Simpokolwe <90185393+luanardev@users.noreply.github.com>
2023-04-30 22:24:12 +0200

    Update model.py

--- model.py
+++ model.py
@@ -1,5 +1,4 @@
 # import libraries
-import os
 import joblib
 import pandas as pd
 import load_data
@@ -19,42 +18,38 @@ df = pd.read_csv(processed_csv)
 # SPLIT DATA FRAME FOR MODEL FITTING
 array = df.values
 X = array[:, 0:3]
-y = array[:, 3]
-X_train, X_validation, Y_train, Y_validation = train_test_split(X, y, test_size=0.15, random_state=1)
-X_train, X_test, Y_train, Y_test = train_test_split(X, y, test_size=0.15, random_state=1)
+Y = array[:, 3]
+x_train, x_validation, y_train, y_validation = train_test_split(X, Y, test_size=0.15, random_state=1)
+x_train, x_test, y_train, y_test = train_test_split(X, Y, test_size=0.15, random_state=1)
 
 # prepare RF model
 model = RandomForestClassifier()
 
 # Make predictions on validation dataset
-model.fit(X_train, Y_train)
-predictions = model.predict(X_validation)
-confusion_matrix(Y_validation, predictions)
+model.fit(x_train, y_train)
+predictions = model.predict(x_validation)
+confusion_matrix(y_validation, predictions)
 
 # OverSampling with SMOTE
 sm = SMOTE(random_state=42)
-X_train_ovr, y_train_ovr = sm.fit_resample(X_train, Y_train.ravel())
+x_train_ovr, y_train_ovr = sm.fit_resample(x_train, y_train.ravel())
 
 # Make predictions on validation dataset
-model.fit(X_train_ovr, y_train_ovr.ravel())
-predictions = model.predict(X_validation)
-confusion_matrix(Y_validation, predictions)
+model.fit(x_train_ovr, y_train_ovr.ravel())
+predictions = model.predict(x_validation)
+confusion_matrix(y_validation, predictions)
 
 # UnderSampling with SMOTE
 under_sm = RandomUnderSampler(random_state=42)
-X_train_undr, y_train_undr = under_sm.fit_resample(X_train, Y_train.ravel())
+x_train_undr, y_train_undr = under_sm.fit_resample(x_train, y_train.ravel())
 
 # Make predictions on validation dataset
-model.fit(X_train_undr, y_train_undr.ravel())
-predictions = model.predict(X_validation)
-confusion_matrix(Y_validation, predictions)
+model.fit(x_train_undr, y_train_undr.ravel())
+predictions = model.predict(x_validation)
+confusion_matrix(y_validation, predictions)
+# %%
 
 # HYPERPARAMETER OPTIMIZATION
-
-# -----RF GRID SEARCH-----
-
-# define evaluation
-cv = RepeatedStratifiedKFold()
 
 # define search space
 space = {
@@ -62,24 +57,37 @@ space = {
     'criterion': ['gini', 'entropy'],
     'max_depth': range(2, 20, 1),
     'min_samples_leaf': [2, 3, 4, 5, 6],
-    'min_samples_split': [1, 2, 5, 4, 6]
+    'min_samples_split': [3, 2, 5, 4, 6]
 }
+	
+# define evaluation
+cv = RepeatedStratifiedKFold()
+
 # define search
 search = GridSearchCV(estimator=model, param_grid=space, scoring='accuracy', cv=3, n_jobs=-1, verbose=2)
+
 # execute search
-result = search.fit(X_train_undr, y_train_undr.ravel())
-# summarize result
-print('.....................................................................\n')
-print("Grid search results for RF")
-print('Best Score: %s' % result.best_score_)
-print('Best Hyper parameters: %s' % result.best_params_)
-print('.....................................................................\n')
+result = search.fit(x_train_undr, y_train_undr.ravel())
+
+# best model parameters
+best = result.best_params_
+
+print("===Best Model Parameters === \n")
+
+# print best 
+print(best)
+
+# %%
+best['max_depth'] = int(best['max_depth']) # convert to int
+best["n_estimators"] = int(best['n_estimators']) # convert to int
+best["min_samples_leaf"] = int(best['min_samples_leaf']) # convert to int
+best["min_samples_split"] = int(best['min_samples_split']) # convert to int
 
 # Rebuild model with optimized parameters
-rfc_model = RandomForestClassifier(criterion='gini', max_depth=2, min_samples_leaf=3, min_samples_split=6, n_estimators=115)
-rfc_model.fit(X_train_undr, y_train_undr.ravel())
-predictions = rfc_model.predict(X_validation)
-confusion_matrix(Y_validation, predictions)
+rfc_model = RandomForestClassifier(**best)
+rfc_model.fit(x_train_undr, y_train_undr.ravel())
+predictions = rfc_model.predict(x_validation)
+confusion_matrix(y_validation, predictions)
 
 # save
 joblib.dump(rfc_model, "rfc_model.joblib")
