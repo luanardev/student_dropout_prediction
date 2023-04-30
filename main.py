5a8779b1d90cb79c226922591acde2e689f10372
Brian Simpokolwe <90185393+luanardev@users.noreply.github.com>
2023-05-01 00:52:55 +0200

    Update main.py

--- main.py
+++ main.py
@@ -6,10 +6,15 @@ import numpy as np
 st.header("Student Dropout Prediction")
 st.subheader("Please select your features!")
 
+#options
+STUDENTTYPE = {1: "MATURED", 2: "GENERIC"}
+EMPLOYMENT = {0: "NO", 2: "YES"}
+WITHDRAWAL = {0: "NO", 1: "YES"}
+
 # get user input
-input_student_type = st.selectbox('What is your student type ? 1=Matured, 2=Generic', [1, 2])
-input_employment = st.selectbox('Are you working or doing business ? 0=No, 1=Yes', [0, 1])
-input_withdrawal = st.selectbox('Did you had any withdrawal in the past ? 0=No, 1=Yes', [0, 1])
+input_student_type = st.selectbox('What is your student type ?', STUDENTTYPE.keys(), format_func=lambda x:STUDENTTYPE[ x ])
+input_employment = st.selectbox('Are you working or doing business ?', EMPLOYMENT.keys(), format_func=lambda x:EMPLOYMENT[ x ])
+input_withdrawal = st.selectbox('Did you withdrawal in the past ?', WITHDRAWAL.keys(), format_func=lambda x:WITHDRAWAL[ x ])
 
 prediction = any;
 
