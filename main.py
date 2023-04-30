c131e6a6d16858850c7c37347b1372f2521fe814
Brian Simpokolwe <90185393+luanardev@users.noreply.github.com>
2023-04-30 23:02:26 +0200

    Update main.py

--- main.py
+++ main.py
@@ -3,7 +3,6 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 
-
 st.header("Student Dropout Prediction")
 
 st.text_input("Enter your Name: ", key="name")
@@ -15,10 +14,9 @@ model = joblib.load("rfc_model.joblib")
 
 st.subheader("Please select your relevant features!")
 
-input_st = st.slider('Student Type(ST)', 0.0, max(data["StudentType(ST)"]), 2.0)
-input_wh = st.slider('Withdrawal History(WH)', 0.0, max(data["Withdrawal History(WH)"]), 1.0)
-input_es = st.slider('Employment Status(ES)', 0.0, max(data["EmploymentStatus(ES)"]), 1.0)
-
+input_st = st.selectBox('What is your student type ?', data["StudentType(ST)"])
+input_es = st.selectBox('What is your employment status ?', data["EmploymentStatus(ES)"])
+input_wh = st.selectBox('Did you had any withdrawal in the past ?', data["WithdrawalHistory(WH)"])
 
 if st.button('Make Prediction'):
     inputs = np.expand_dims([input_st, input_wh, input_es], 0)
@@ -32,6 +30,3 @@ elif prediction == 0:
     print("Student is not likely to dropout")
 
 st.write(f"Thank you {st.session_state.name}! I hope you liked it.")
-
-
-
