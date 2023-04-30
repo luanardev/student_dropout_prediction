1982aca00e09b84f7933d6f18a634c0f5a03fc28
Brian Simpokolwe <90185393+luanardev@users.noreply.github.com>
2023-04-30 22:24:06 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,48 +1,35 @@
+import joblib
 import streamlit as st
 import pandas as pd
-from sklearn.model_selection import train_test_split
-from sklearn.preprocessing import  LabelEncoder
-import xgboost as xgb
 import numpy as np
-st.header("Fish Weight Prediction App")
+
+
+st.header("Student Dropout Prediction")
+
 st.text_input("Enter your Name: ", key="name")
-data = pd.read_csv("https://raw.githubusercontent.com/gurokeretcha/WishWeightPredictionApplication/master/Fish.csv")
-#load label encoder
-encoder = LabelEncoder()
-encoder.classes_ = np.load('classes.npy',allow_pickle=True)
+
+data = pd.read_csv("processed_data.csv")
 
 # load model
-best_xgboost_model = xgb.XGBRegressor()
-best_xgboost_model.load_model("best_model.json")
+model = joblib.load("rfc_model.joblib")
 
-if st.checkbox('Show Training Dataframe'):
-    data
+st.subheader("Please select your relevant features!")
 
-st.subheader("Please select relevant features of your fish!")
-left_column, right_column = st.columns(2)
-with left_column:
-    inp_species = st.radio(
-        'Name of the fish:',
-        np.unique(data['Species']))
-
-
-input_Length1 = st.slider('Vertical length(cm)', 0.0, max(data["Length1"]), 1.0)
-input_Length2 = st.slider('Diagonal length(cm)', 0.0, max(data["Length2"]), 1.0)
-input_Length3 = st.slider('Cross length(cm)', 0.0, max(data["Length3"]), 1.0)
-input_Height = st.slider('Height(cm)', 0.0, max(data["Height"]), 1.0)
-input_Width = st.slider('Diagonal width(cm)', 0.0, max(data["Width"]), 1.0)
-
+input_st = st.slider('Student Type(ST))', 0.0, max(data["StudentType(ST)"]), 1.0)
+input_wh = st.slider('Withdrawal History(WH)', 0.0, max(data["Withdrawal History(WH)"]), 1.0)
+input_es = st.slider('Employment Status(ES)', 0.0, max(data["EmploymentStatus(ES)"]), 1.0)
 
 if st.button('Make Prediction'):
-    input_species = encoder.transform(np.expand_dims(inp_species, -1))
-    inputs = np.expand_dims(
-        [int(input_species), input_Length1, input_Length2, input_Length3, input_Height, input_Width], 0)
-    prediction = best_xgboost_model.predict(inputs)
-    print("final pred", np.squeeze(prediction, -1))
-    st.write(f"Your fish weight is: {np.squeeze(prediction, -1):.2f}g")
+    inputs = np.expand_dims([input_st, input_wh, input_es], 0)
+    prediction = model.predict(inputs)
+    print("Predicted Target = ", np.squeeze(prediction, -1))
+
+	if prediction == 1:
+		print("Student is likely to dropout")
+	elif prediction == 0:
+		print("Student is not likely to dropout")
 
     st.write(f"Thank you {st.session_state.name}! I hope you liked it.")
-    st.write(f"If you want to see more advanced applications you can follow me on [medium](https://medium.com/@gkeretchashvili)")
 
 
 
