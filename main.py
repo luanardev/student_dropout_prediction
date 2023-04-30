5362907b180065250d33490398ecfd84ce0e6ab1
Brian Simpokolwe <90185393+luanardev@users.noreply.github.com>
2023-05-01 00:16:53 +0200

    Update main.py

--- main.py
+++ main.py
@@ -2,29 +2,31 @@ import joblib
 import streamlit as st
 import pandas as pd
 import numpy as np
+import load_data
 
 st.header("Student Dropout Prediction")
-
-st.text_input("Enter your Name: ", key="name")
 
 # load model
 model = joblib.load("rfc_model.joblib")
 
 st.subheader("Please select your relevant features!")
 
-input_st = st.selectBox('What is your student type ?', ("Generic", "Matured"))
-input_es = st.selectBox('Are you working or doing business ?', ("Yes", "No"))
-input_wh = st.selectBox('Did you had any withdrawal in the past ?', ("Yes", "No"))
+# get user input
+input_student_type = st.selectbox('What is your student type ? 1=Matured, 2=Generic', [1, 2])
+input_employment = st.selectbox('Are you working or doing business ? 0=No, 1=Yes', [0, 1])
+input_withdrawal = st.selectbox('Did you had any withdrawal in the past ? 0=No, 1=Yes', [0, 1])
+
+prediction = any;
 
 if st.button('Make Prediction'):
-    inputs = np.expand_dims([input_st, input_wh, input_es], 0)
+
+    inputs = np.expand_dims([input_withdrawal, input_student_type, input_employment], 0)
     prediction = model.predict(inputs)
-    print("Predicted Target = ", np.squeeze(prediction, -1))
-
+    st.write(f"Predicted Target is {np.squeeze(prediction, -1)}")
+	
 if prediction == 1:
-    print("Student is likely to dropout")
-
+	st.write("Student is likely to dropout")  
+	
 elif prediction == 0:
-    print("Student is not likely to dropout")
-
-st.write(f"Thank you {st.session_state.name}! I hope you liked it.")
+	st.write("Student is not likely to dropout")
+	
