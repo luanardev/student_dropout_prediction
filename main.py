1b03c89440883acd97761d1c7422b16cc0fa5631
Brian Simpokolwe <90185393+luanardev@users.noreply.github.com>
2023-04-30 22:33:15 +0200

    Update main.py

--- main.py
+++ main.py
@@ -19,17 +19,19 @@ input_st = st.slider('Student Type(ST))', 0.0, max(data["StudentType(ST)"]), 1.0
 input_wh = st.slider('Withdrawal History(WH)', 0.0, max(data["Withdrawal History(WH)"]), 1.0)
 input_es = st.slider('Employment Status(ES)', 0.0, max(data["EmploymentStatus(ES)"]), 1.0)
 
+
 if st.button('Make Prediction'):
     inputs = np.expand_dims([input_st, input_wh, input_es], 0)
     prediction = model.predict(inputs)
     print("Predicted Target = ", np.squeeze(prediction, -1))
 
-	if prediction == 1:
-		print("Student is likely to dropout")
-	elif prediction == 0:
-		print("Student is not likely to dropout")
+if prediction == 1:
+    print("Student is likely to dropout")
 
-    st.write(f"Thank you {st.session_state.name}! I hope you liked it.")
+elif prediction == 0:
+    print("Student is not likely to dropout")
+
+st.write(f"Thank you {st.session_state.name}! I hope you liked it.")
 
 
 
