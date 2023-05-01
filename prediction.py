bcb1796a5e671f26ee5494892210de3a21fa9c05
Brian Simpokolwe <90185393+luanardev@users.noreply.github.com>
2023-05-01 11:33:09 +0200

    Update prediction.py

--- prediction.py
+++ prediction.py
@@ -6,18 +6,18 @@ from sklearn.model_selection import train_test_split
 data = pd.read_csv('processed_data.csv')
 
 array = data.values
-X = array[:, 0:3]
-Y = array[:, 3]
-X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size=0.15, random_state=1)
+X = array[:, 0:7]
+Y = array[:, 7]
+x_train, x_test, y_train, y_test = train_test_split(X, Y, test_size=0.15, random_state=1)
 
 # load model
 print("====== Loading Model ======= \n")
 rfc_model = joblib.load("rfc_model.joblib")
-pred = rfc_model.predict(X_train)
+pred = rfc_model.predict(x_train)
 
 # %%
 # test data 
-inputs = np.expand_dims([1,1,1],0)
+inputs =  np.expand_dims(x_test[0], 0)
 
 print("===== Predicting ==== \n")
 prediction = rfc_model.predict(inputs)
