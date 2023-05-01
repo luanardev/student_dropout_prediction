6b5ce717ae853316b0ac82d6f305cefa0343f995
Brian Simpokolwe <90185393+luanardev@users.noreply.github.com>
2023-05-01 11:33:01 +0200

    Update main.py

--- main.py
+++ main.py
@@ -10,17 +10,33 @@ st.subheader("Please select your features!")
 STUDENTTYPE = {1: "MATURED", 2: "GENERIC"}
 EMPLOYMENT = {0: "NO", 1: "YES"}
 WITHDRAWAL = {0: "NO", 1: "YES"}
+REPEAT = {0: "NO", 1: "YES"}
+MARITALSTATUS = {4: "Male", 2: "Female"}
+GENDER = {2: "Male", 1: "Female"}
+
 
 # get user input
 input_student_type = st.selectbox('What is your student type ?', STUDENTTYPE.keys(), format_func=lambda x:STUDENTTYPE[ x ])
+input_gender = st.selectbox('What is your gender ?', GENDER.keys(), format_func=lambda x:GENDER[ x ])
+input_age = st.text_input('What is your age ?')
+input_marital = st.selectbox('What is your marital status ?', MARITALSTATUS.keys(), format_func=lambda x:MARITALSTATUS[ x ])
 input_employment = st.selectbox('Are you working or doing business ?', EMPLOYMENT.keys(), format_func=lambda x:EMPLOYMENT[ x ])
 input_withdrawal = st.selectbox('Did you withdrawal in the past ?', WITHDRAWAL.keys(), format_func=lambda x:WITHDRAWAL[ x ])
+input_repeat = st.selectbox('Did you repeat in the past ?', REPEAT.keys(), format_func=lambda x:REPEAT[ x ])
 
 prediction = any
 
 if st.button('Make Prediction'):
 
-    inputs = np.expand_dims([input_withdrawal, input_student_type, input_employment], 0)
+    inputs = np.expand_dims([
+	    input_withdrawal, 
+	    input_repeat, 
+	    input_marital, 
+	    input_gender,
+	    input_student_type, 
+	    input_age,
+	    input_employment], 0)
+    
     model = joblib.load("rfc_model.joblib")
     prediction = model.predict(inputs)
     st.write(f"Predicted Target is {np.squeeze(prediction, -1)}")
